9a5ed6f5d69ca940d7f9083f09e6fc42995f613f
witzawitz <witzawitz@gmail.com>
2016-04-11 22:45:24 +0300

    Ignore DB config.

--- tests/config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-	return [
-		'class'    => 'yii\db\Connection',
-		'dsn'      => 'mysql:host=localhost;dbname=vps_tools_tests',
-		'username' => 'vps_tools_tests',
-		'password' => 'UU7tTZyZtTQGYDcG',
-		'charset'  => 'utf8',
-	];
